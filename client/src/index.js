d1e2b32877bca0940dc7bc4c3a6d4eab3a998990
mkor <31717889+mkora@users.noreply.github.com>
2018-07-06 17:01:05 -0700

    Fix paths

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './css/index.css';
+import App from './containers/App';
+import registerServiceWorker from './utils/registerServiceWorker';
 import 'typeface-roboto'
 
 ReactDOM.render(<App />, document.getElementById('root'));
